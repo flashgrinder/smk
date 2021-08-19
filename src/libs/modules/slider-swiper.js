ac5b014c2894d9742df090fb4d581bfede271f08
flashgrinder <flashgrinder92@gmail.com>
2021-08-19 16:08:53 +0400

    wip services-slider

--- src/libs/modules/slider-swiper.js
+++ src/libs/modules/slider-swiper.js
@@ -1,17 +1,55 @@
-import Swiper from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 
 function init() {
+
+    Swiper.use([Navigation]);
+
     const servicesSlider = new Swiper(".services-slider__swiper", {
-        slidesPerView: 4,
-        // slidesPerColumn: 2,
-        // slidesPerColumnFill: 'row',
-        spaceBetween: 20,
-        freeMode: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        slidesPerColumnFill: 'row',
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.services-slider__arrow--next',
+            prevEl: '.services-slider__arrow--prev'
         },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                slidesPerColumn: 2,
+                spaceBetween: 10
+            },
+            576: {
+                slidesPerView: 3,
+                slidesPerColumn: 2,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 3,
+                slidesPerColumn: 2,
+                spaceBetween: 20
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+                slidesPerColumn: 1
+            }
+        }
     });
+
+    // const swiperPrev = document.querySelector('.services-slider__arrow--prev')
+    // const swiperNext = document.querySelector('.services-slider__arrow--next')
+
+    // swiperPrev.addEventListener('click', () => {
+    //     servicesSlider.slidePrev();
+    // })
+    // swiperNext.addEventListener('click', () => {
+    //     servicesSlider.slideNext();
+    // })
 }
 
 export default { init }
