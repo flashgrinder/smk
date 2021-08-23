4b4b70da99e2fc9a40389ab44889dd7ea3d03ed6
flashgrinder <flashgrinder92@gmail.com>
2021-08-23 11:48:02 +0400

    wip tooltip modal

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,11 +1,13 @@
 import burger from '../libs/modules/burger-menu.js';
 import servicesSlider from '../libs/modules/slider-swiper.js';
 import modal from '../libs/modules/modal.js';
+import tooltipModal from '../libs/modules/tooltip-modal.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
     
     burger.init();
     modal.init();
     servicesSlider.init();
+    tooltipModal.init();
     
 });
