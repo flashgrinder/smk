5fdec395703170f436666c2ebc870a7f778a413d
flashgrinder <flashgrinder92@gmail.com>
2021-09-11 23:59:33 +0400

    fix script reveal

--- src/libs/modules/reveal.js
+++ src/libs/modules/reveal.js
@@ -3,7 +3,7 @@ function init() {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 revealItemsObserver.unobserve(entry.target);
-                entry.target.classList.add('is-reveal');
+                entry.target.classList.add('is-revealed');
             }
         });
     });
--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -2,8 +2,7 @@ import burger from '../libs/modules/burger-menu.js';
 import servicesSlider from '../libs/modules/slider-swiper.js';
 import modal from '../libs/modules/modal.js';
 import tooltipModal from '../libs/modules/tooltip-modal.js';
-import reveal from '.../modules/reveal';
-// import rangeCount from '../libs/modules/range-count.js';
+import reveal from '../libs/modules/reveal.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
     burger.init();
@@ -11,28 +10,5 @@ document.addEventListener('DOMContentLoaded', function(e) {
     servicesSlider.init();
     tooltipModal.init();
     reveal.init();
-
-    // rangeCount.init();
-
-    // const rangeSliders = document.querySelector(".shop-card__range");
-    // const output = document.querySelector(".shop-card__field");
-
-    // output.setAttribute('value', '')
-
-    // output.innerHTML = rangeSliders.value; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    // rangeSliders.oninput = function() {
-    //     output.innerHTML = this.setAttribute(value, this.value);
-    // }
-
-    // rangeSliders.forEach(el => {
-    //     // console.log(el);
-    //     // console.log(rangeSliders);
-    //     el.addEventListener('oninput', function() {
-    //         output.innerHTML = this.value;
-    //     })
-    //     console.log(output)
-    // })
     
 });
