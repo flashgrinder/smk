cda7a8afa73076d0df9134b6273f9660d66c26bb
flashgrinder <flashgrinder92@gmail.com>
2021-09-16 12:25:42 +0400

    del fiter ctf7

--- inc/filters.php
+++ inc/filters.php
@@ -1,13 +1,4 @@
 <?php
-
-    /* Удаляем лишние обёртки у Contact Form 7 */
-    // add_filter( 'wpcf7_form_elements', function($content) {
-
-    //     $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
-
-    //     return $content;
-        
-    // });
     
     /* Убираем тег <p></p> у отрывков */
     remove_filter( 'the_excerpt','wpautop');
