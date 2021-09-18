d2564bdbd0f9b9bea84f6ced855563f3395aaf55
flashgrinder <flashgrinder92@gmail.com>
2021-09-18 16:51:15 +0400

    test tax

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -49,9 +49,48 @@ defined( 'ABSPATH' ) || exit;
 
         <div class="services-store__subtotal cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
 
+            <!-- Test -->
+            <?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+			<tr class="fee">
+				<th><?php echo esc_html( $fee->name ); ?></th>
+				<td data-title="<?php echo esc_attr( $fee->name ); ?>"><?php wc_cart_totals_fee_html( $fee ); ?></td>
+			</tr>
+            <?php endforeach; ?>
+
+            <?php
+            if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
+                $taxable_address = WC()->customer->get_taxable_address();
+                $estimated_text  = '';
+
+                if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
+                    /* translators: %s location. */
+                    $estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
+                }
+
+                if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
+                    foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+                        ?>
+                        <tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+                            <th><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
+                            <td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+                        </tr>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <tr class="tax-total">
+                        <th><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
+                        <td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
+                    </tr>
+                    <?php
+                }
+            }
+            ?>
+            <!-- Test -->
+
             <?php do_action( 'woocommerce_cart_totals_before_order_total' ); ?>
 
-            <span class="services-store__subtotal-text text text--large text--white text--w-regular">
+            <span class="services-store__subtotal-text text text--large text--white text--w-regular order-total">
                 <?php esc_html_e( 'Total', 'woocommerce' ); ?>:
             </span>
             <span data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>" class="services-store__subtotal-price text text--white text--w-bold">
--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -54,6 +54,31 @@ defined( 'ABSPATH' ) || exit;
 
         ?>
 
+        <!-- Test -->
+        <?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+			<tr class="fee">
+				<th><?php echo esc_html( $fee->name ); ?></th>
+				<td><?php wc_cart_totals_fee_html( $fee ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+        <?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
+			<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
+				<?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited ?>
+					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+						<th><?php echo esc_html( $tax->label ); ?></th>
+						<td><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<tr class="tax-total">
+					<th><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
+					<td><?php wc_cart_totals_taxes_total_html(); ?></td>
+				</tr>
+			<?php endif; ?>
+		<?php endif; ?>
+        <!-- Test -->
+
         <?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
 
         <div class="checkout-form__product-row checkout-form__product-row--last">
